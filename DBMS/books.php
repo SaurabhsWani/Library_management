20aadd522291d1f164640bb3963c3bb0cd4790a8
SaurabhWani <er.saurabhwani1@gmail.com>
2020-12-30 11:53:23 +0530

    V20

--- DBMS/books.php
+++ DBMS/books.php
@@ -11,7 +11,7 @@ title("Books");
     <div class="widget-content">
       <div class="shortcuts"> 
         <a href="#myModal" role="button" data-toggle="modal" class="shortcut"><i class="shortcut-icon icon-plus-sign"></i><span class="shortcut-label">Add Book</span> </a>
-        <a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-upload-alt"></i> <span class="shortcut-label">Import Books Data</span> </a>
+        <a href="#myModal3" role="button" data-toggle="modal" class="shortcut"><i class="shortcut-icon icon-upload-alt"></i><span class="shortcut-label">Upload Book Data</span> </a>
         <a href="#myModal2" role="button" data-toggle="modal" class="shortcut"><i class="shortcut-icon icon-remove-sign"></i> <span class="shortcut-label">Remove Book</span> </a>
         <a href="#myModal0" role="button" data-toggle="modal" class="shortcut"><i class="shortcut-icon icon-search"></i> <span class="shortcut-label">Search Book</span> </a>
       </div>
@@ -20,10 +20,10 @@ title("Books");
     <!-- /widget-content --> 
   </div>
   <!-- /widget -->
+<?php message();?>
 
-
-<!-- Add books -->
-<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <!-- Add books -->
+  <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-header">
       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
       <h3 id="myModalLabel">Add New Book</h3>
@@ -58,8 +58,8 @@ title("Books");
       </form>
     </div>
   </div>
-<!-- Remove Books -->
-<div id="myModal2" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <!-- Remove Books -->
+  <div id="myModal2" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-header">
       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
       <h3 id="myModalLabel">Remove Book</h3>
@@ -79,77 +79,108 @@ title("Books");
       </form>
     </div>
   </div>
-<!-- search Books -->
-<div id="myModal0" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
+  <!-- Upload data to db -->
+  <div id="myModal3" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+      <h3 id="myModalLabel">Upload Book Data</h3>
+    </div>
+    <div class="modal-body">
+      <div class="row" align="center">
+        <!-- Import link -->
+        <div class="col-md-12 head">
+          <div class="float-right">
+            <a href="javascript:void(0);" class="btn btn-success" onclick="formToggle('importFrm');"><i class="plus"></i> Upload  Book Record</a>
+          </div>
+        </div>
+        <!-- CSV file upload form -->
+        <div class="col-md-12" id="importFrm" style="display: none;">
+          <form action="importdata.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="file" />
+            <input type="submit" class="btn btn-primary" name="importbk" value="Upload Book Data">
+          </form>
+        </div>
+      </div><br>
+
+      <div class="row" align="center">
+        <!-- Import link -->
+        <div class="col-md-12 head">
+          <div class="float-right">
+            <a href="javascript:void(0);" class="btn btn-success" onclick="formToggle('importFrm1');"><i class="plus"></i> Upload record of copies</a>
+          </div>
+        </div><br>
+        <div class="col-md-12" id="importFrm1" style="display: none;">
+          <form action="importdata.php" method="post">
+            <input type="file" name="file" />
+            <input type="submit" class="btn btn-primary" name="importcp" value="Upload Copy Data">
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>  <!-- /controls -->      
+
+
+  <!-- search Books -->
+  <div id="myModal0" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-header">
       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
       <h3 id="myModalLabel">Search Book</h3>
     </div>
     <div class="modal-body">
       <div class="row" align="center">
-      <!-- Import link -->
-    <div class="col-md-12 head">
-        <div class="float-right">
-            <a href="javascript:void(0);" class="btn btn-success" onclick="formToggle('importFrm');"><i class="plus"></i> Search by ID</a>
-        </div>
-      </div><br>
-        <div class="col-md-12" id="importFrm" style="display: none;">
-        <form action="srch.php" method="post">
+        <!-- Import link -->
+        <div class="col-md-12 head">
+          <div class="float-right">
+            <a class="btn btn-success" onclick="formToggle('sbi');"><i class="plus"></i> Search by ID</a>
+          </div>
+        </div><br>
+        <div class="col-md-12" id="sbi" style="display: none;">
+          <form action="srch.php" method="post">
             <input type="text" name="sbid" />
             <Button class="btn" type="submit" name="searchbooks" value="sbidbt" class="btn btn-primary">Search</Button>
-        </form>
-    </div>
-    </div>
-
-    <div class="row" align="center">
-      <!-- Import link -->
-    <div class="col-md-12 head">
-        <div class="float-right">
-            <a href="javascript:void(0);" class="btn btn-success" onclick="formToggle('importFrm1');"><i class="plus"></i> Search by Book Name</a>
+          </form>
         </div>
-      </div><br>
-        <div class="col-md-12" id="importFrm1" style="display: none;">
-        <form action="srch.php" method="post">
+      </div>
+
+      <div class="row" align="center">
+        <!-- Import link -->
+        <div class="col-md-12 head">
+          <div class="float-right">
+            <a class="btn btn-success" onclick="formToggle('sbn');"><i class="plus"></i> Search by Book Name</a>
+          </div>
+        </div><br>
+        <div class="col-md-12" id="sbn" style="display: none;">
+          <form action="srch.php" method="post">
             <input type="text" name="sbbn" />
             <Button class="btn" type="submit" name="searchbooks" value="sbbnbt" class="btn btn-primary">Search</Button>
-        </form>
-    </div>
-    </div>
-
-    <div class="row" align="center">
-      <!-- Import link -->
-    <div class="col-md-12 head">
-        <div class="float-right">
-            <a href="javascript:void(0);" class="btn btn-success" onclick="formToggle('importFrm2');"><i class="plus"></i> Search by Auther</a>
+          </form>
         </div>
-      </div><br>
-        <div class="col-md-12" id="importFrm2" style="display: none;">
-        <form action="srch.php" method="post">
+      </div>
+
+      <div class="row" align="center">
+        <!-- Import link -->
+        <div class="col-md-12 head">
+          <div class="float-right">
+            <a class="btn btn-success" onclick="formToggle('sba');"><i class="plus"></i> Search by Auther</a>
+          </div>
+        </div><br>
+        <div class="col-md-12" id="sba" style="display: none;">
+          <form action="srch.php" method="post">
             <input type="text" name="sba" />
             <Button class="btn" type="submit" name="searchbooks" value="sbabt" class="btn btn-primary">Search</Button>
-        </form>
+          </form>
+        </div>
+      </div>
     </div>
-    </div>
-
-    <script>
-function formToggle(ID){
-    var element = document.getElementById(ID);
-    if(element.style.display === "none"){
-        element.style.display = "block";
-    }else{
-        element.style.display = "none";
-    }
-}
-</script>
-  </div>
-</div> 
-<div class="widget">
+  </div> 
+  <div class="widget">
     <div class="widget-header"> <i class='icon-list'></i> 
       <h3><?php
       $result=select("*","books","WHERE 1");
       $row=mysqli_num_rows($result);
       if($row!=0){ ?>
-          Book List 
+        Book List 
       <?php } ?>
     </h3>
   </div>
@@ -161,7 +192,7 @@ function formToggle(ID){
       <table class="table table-striped">
         <thead>
           <tr>
-            <th><center>Book ID</center></th>
+            <th><center>Referance ID</center></th>
             <th><center>Book Name</center></th>
             <th><center>Auther</center></th>
             <th><center>Total Copies</center></th>
@@ -178,21 +209,30 @@ function formToggle(ID){
             <td><center> ".$std['book_name']."</center></td>  
             <td><center> ".$std['auther']."</center></td>  
             <td><center> ".$std['toatal_cp']."</center></td>
-              <td><center> ".$std['cp_left']."</center></td>     
+            <td><center> ".$std['cp_left']."</center></td>     
             <td><center>
             <b> ";?>
             <?php 
-              echo "</a></center></td>
-              </tr>";
-            }
-            ?>
-          </tbody>
-        </table>
-      </div>
+            echo "</a></center></td>
+            </tr>";
+          }
+          ?>
+        </tbody>
+      </table>
     </div>
-    <!-- /widget-content --> 
   </div>
-
+  <!-- /widget-content --> 
+</div>
+<script>
+  function formToggle(ID){
+    var element = document.getElementById(ID);
+    if(element.style.display === "none"){
+      element.style.display = "block";
+    }else{
+      element.style.display = "none";
+    }
+  }
+</script>
 <?php
 include('footer.php');
 ?>
--- DBMS/student.php
+++ DBMS/student.php
@@ -123,7 +123,7 @@ title("Student");
         <div class="col-md-12" id="importFrm" style="display: none;">
           <form action="importdata.php" method="post" enctype="multipart/form-data">
             <input type="file" name="file" />
-            <input type="submit" class="btn btn-primary" name="importSubmit" value="IMPORT">
+            <input type="submit" class="btn btn-primary" name="importSubmit" value="Upload">
           </form>
         </div>
       </div>
@@ -149,6 +149,7 @@ title("Student");
   </div>
   <div class="modal-body">
     <form  name="search" action="filter.php?page=Dashboard&cnt=800" method="POST" class="form-horizontal" >
+      <input type="hidden" value="std" name="cat">
         <table class="table table-striped table-bordered">
           <thead>
             <tr>
